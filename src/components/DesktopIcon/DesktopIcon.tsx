0b281e16735d5323ad68b15b3acb912baeabbdf9
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-04-27 18:03:58 +0700

    fix

--- src/components/DesktopIcon/DesktopIcon.tsx
+++ src/components/DesktopIcon/DesktopIcon.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function DesktopIcon(props) {
   return <div {...props} className="absolute group cursor-pointer overflow-hidden select-none flex flex-col items-center desktop-icon-appear" style={{ top: props.top, left: props.left, width: 100 }}>
     <div className="bg-color5 opacity-0 group-hover:opacity-30 w-full h-full absolute" style={{ zIndex: 0 }} ></div>
-    <Image src={props.icon} alt={"dekstop-icon"} className="shadow-2xl mt-1"  width={45} height={45} style={{ zIndex: 1, imageRendering: "pixelated" }}></Image>
+    <Image src={props.icon} alt="dekstop-icon" className="shadow-2xl mt-1" width={45} height={45} style={{ zIndex: 1, imageRendering: "pixelated" }}></Image>
     {/* <img alt={"dekstop-icon"} src={props.icon} className="shadow-2xl mt-1" style={{ zIndex: 1, imageRendering: "pixelated", width: 45, height: 45 }}></img> */}
     <p className="mt-1 text-white text-center" style={{ zIndex: 1 }} >{(props.title.length > 13) ? props.title.substring(0, 11) + "..." : props.title}</p>
   </div>
