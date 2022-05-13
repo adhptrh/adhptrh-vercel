ee054cc02c3e222d8e13bca115733f6e29ddcd27
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-05-13 20:52:39 +0700

    autplay vid

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -3,7 +3,7 @@ import Container from "../Container/Container";
 export default function Video(props) {
     return <Container {...props} title={props.title}>
     <div className="flex w-full h-full">
-      <video width="100%" controls>
+      <video autoPlay width="100%" controls>
         <source src="video.mp4" type="video/mp4"/>
       Your browser does not support the video tag.
       </video>
